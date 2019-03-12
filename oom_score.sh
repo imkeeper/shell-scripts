20a3ee87a1067c723ee817be6855700915726053
junyi <qiongzhetong@163.com>
2019-03-12 14:20:03 +0800

    Create oom_score.sh
    打印当前系统上 oom_score 分数最高（最容易被 OOM Killer 杀掉）的进程

---
+++ oom_score.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#打印当前系统上 oom_score 分数最高（最容易被 OOM Killer 杀掉）的进程
+for proc in $(find /proc -maxdepth 1 -regex '/proc/[0-9]+'); do
+    printf "%2d %5d %s\n" \
+        "$(cat $proc/oom_score)" \
+        "$(basename $proc)" \
+        "$(cat $proc/cmdline | tr '\0' ' ' | head -c 50)"
+done 2>/dev/null | sort -nr | head -n 10
